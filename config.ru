b5c39fc7f50393cd3482f93be45f836e775b181f
Cat <ccallcottstevens@gmail.com>
2021-03-09 14:57:00 +1100

    deleted scaffold

--- config.ru
+++ config.ru
@@ -1,6 +1,6 @@
 # This file is used by Rack-based servers to start the application.
 
 require_relative "config/environment"
-
+Rack::MiniProfiler.config.position = 'bottom-right'
 run Rails.application
 Rails.application.load_server
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_002958) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "listings", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.decimal "price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
